cb5afe309655e7f18608bb288374e00965c7029d
DanielGCG <danielgcg@dcc.ufrj.br>
2024-08-29 21:55:48 -0300

    a

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const postsController = require('../controllers/postsController');
 
-// Rota para criar uma nova postagem
-router.post('/', postsController.createPost);
+// Array simples para armazenar os posts temporariamente
+let posts = [];
 
-// Rota para listar todas as postagens
-router.get('/', postsController.getAllPosts);
+// Rota para obter todos os posts
+router.get('/', (req, res) => {
+    res.json(posts);
+});
+
+// Rota para criar um novo post
+router.post('/', (req, res) => {
+    const { title, content } = req.body;
+    const newPost = { id: posts.length + 1, title, content };
+    posts.push(newPost);
+    res.status(201).json(newPost);
+});
+
+// Rota para deletar um post pelo ID
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    posts = posts.filter(post => post.id !== parseInt(id, 10));
+    res.status(204).end();
+});
 
 module.exports = router;
